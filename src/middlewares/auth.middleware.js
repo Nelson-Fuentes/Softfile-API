1840f3676e1d749d9584e0111109c38d24af9466
Nelson-Fuentes <nelsonafp12@gmail.com>
2021-02-03 20:29:33 -0500

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -24,6 +24,6 @@ export const verify_user_from_token = async(request, response, next) => {
     if (!user) {
         response.status(401).json({ message: 'La sesion no corresponde a ningun usuario activo.' });
     } else {
-        response.status(200).json({ message: 'Acceso otorgado' });
+        response.status(200).json({ username: user.username, firstname: user.firstname, lastname: user.lastname });
     }
 }
