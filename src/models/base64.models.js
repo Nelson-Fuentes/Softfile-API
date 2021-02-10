db1a8a85476617b73f917b193eb4aa530c105a10
Nelson-Fuentes <nelsonafp12@gmail.com>
2021-02-10 17:29:14 -0500

    Update base64.models.js

--- src/models/base64.models.js
+++ src/models/base64.models.js
@@ -4,10 +4,12 @@ export default class base64string {
             this.string = string;
             const matches = this.string.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
                 response = {};
-            this.is_valid = matches.length == 3;
-            this.data = matches[2];
-            this.type = matches[1].split('/')[0];
-            this.extension = matches[1].split('/')[1]
+            this.is_valid = matches & matches.length == 3;
+            if (this.is_valid) {
+                this.data = matches[2];
+                this.type = matches[1].split('/')[0];
+                this.extension = matches[1].split('/')[1]
+            }
         } else {
             this.is_valid = false;
         }
