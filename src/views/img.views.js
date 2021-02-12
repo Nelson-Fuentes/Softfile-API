72c7f51a6724075773296fa5576f31592f51aab7
Nelson-Fuentes <nelsonafp12@gmail.com>
2021-02-11 20:13:20 -0500

    Update img.views.js

--- src/views/img.views.js
+++ src/views/img.views.js
@@ -3,6 +3,7 @@ import express from 'express';
 
 export const view_image = async(request, response) => {
     const filename = request.params.filename;
-    const path_image = __dirname.replace("src/views", '') + FileController.PATH_UPLOADED_FILES + FileController.PATH_UPLOADED_IMAGES + filename;
+    let path_image = __dirname.replace("build", 'src');
+    path_image = path_image.replace("src/views", '') + FileController.PATH_UPLOADED_FILES + FileController.PATH_UPLOADED_IMAGES + filename;
     response.sendFile(path_image);
 }
